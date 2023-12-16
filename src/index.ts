2a66f3270f54b5fa7341f0d0ac981834f3cdbe9b
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-12-16 22:08:38 +0900

    add discord client

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import { Elysia } from "elysia";
+import { Client, Events, GatewayIntentBits } from 'discord.js';
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
 );
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+// listen for the client to be ready
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
+
