bf5538a39112fe635ceec434da15a133f4689a40
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-12-16 22:11:27 +0900

    PORT -> env

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { Elysia } from "elysia";
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get("/", () => "Hello Elysia").listen(process.env.PORT ?? 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
