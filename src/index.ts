676092dd02d0e39f992e826e1f66d317b94612b5
RUNFUNRUN <ryotauchiyama1212@gmail.com>
2023-12-17 18:52:13 +0900

    add ,

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { app } from './elysia';
 app.listen(process.env.PORT ?? 3000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
 
 client.login(process.env.DISCORD_TOKEN);
